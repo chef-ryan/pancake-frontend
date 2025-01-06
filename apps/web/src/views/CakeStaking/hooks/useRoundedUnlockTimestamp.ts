7ac92c0c298df6181ae8e30b8cd1153f6b7e9955
memoyil <2213635+memoyil@users.noreply.github.com>
2025-01-06 14:10:43 +0100

    fix: Cakestaking rounded unlock timestamp error

--- apps/web/src/views/CakeStaking/hooks/useRoundedUnlockTimestamp.ts
+++ apps/web/src/views/CakeStaking/hooks/useRoundedUnlockTimestamp.ts
@@ -1,22 +1,31 @@
 import { MAX_VECAKE_LOCK_WEEKS, WEEK } from 'config/constants/veCake'
 import dayjs from 'dayjs'
 import { useLockCakeData } from 'state/vecake/hooks'
+import { useMemo } from 'react'
 import { useCurrentBlockTimestamp } from './useCurrentBlockTimestamp'
 
 export const useRoundedUnlockTimestamp = (startTimestamp?: number): bigint | undefined => {
   const { cakeLockWeeks } = useLockCakeData()
   const currentTimestamp = useCurrentBlockTimestamp()
 
-  const week = Number(cakeLockWeeks) || 0
+  const roundUnlockTimestamp = useMemo(() => {
+    const week = Number(cakeLockWeeks) || 0
 
-  const maxUnlockTimestamp = dayjs.unix(currentTimestamp).add(MAX_VECAKE_LOCK_WEEKS, 'weeks').unix()
-  const userUnlockTimestamp = dayjs
-    .unix(startTimestamp || currentTimestamp)
-    .add(week, 'weeks')
-    .unix()
+    const maxUnlockTimestamp = dayjs.unix(currentTimestamp).add(MAX_VECAKE_LOCK_WEEKS, 'weeks').unix()
+    const userUnlockTimestamp = dayjs
+      .unix(startTimestamp || currentTimestamp)
+      .add(week, 'weeks')
+      .unix()
 
-  const unlockTimestamp = userUnlockTimestamp > maxUnlockTimestamp ? maxUnlockTimestamp : userUnlockTimestamp
-  const roundUnlockTimestamp = BigInt(Math.floor(unlockTimestamp / WEEK) * WEEK)
+    const unlockTimestamp = userUnlockTimestamp > maxUnlockTimestamp ? maxUnlockTimestamp : userUnlockTimestamp
+    const flooredUnlockTimestamp = Math.floor(unlockTimestamp / WEEK) * WEEK
+
+    if (!flooredUnlockTimestamp) {
+      return undefined
+    }
+
+    return BigInt(flooredUnlockTimestamp)
+  }, [cakeLockWeeks, startTimestamp, currentTimestamp])
 
   return roundUnlockTimestamp
 }
