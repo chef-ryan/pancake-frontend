d7f0a7320e4e3fd97635a75dc48fcff1c26c4154
Chef Penguin <penguin@pancakeswap.com>
2025-01-09 10:31:38 +0530

    chore: update jetton

--- packages/routing-sdk/addons/ton/src/Jetton.ts
+++ packages/routing-sdk/addons/ton/src/Jetton.ts
@@ -4,8 +4,4 @@ export class Jetton extends AgnosticToken {
   public sortsBefore(other: Jetton): boolean {
     return super.sortsBefore(other)
   }
-
-  public equals(other: Jetton): boolean {
-    return this.chainId === other.chainId && this.address === other.address
-  }
 }
--- packages/routing-sdk/addons/ton/src/currency.ts
+++ packages/routing-sdk/addons/ton/src/currency.ts
@@ -1,5 +1,5 @@
 import { AgnosticNativeCurrency } from './AgnosticNativeCurrency'
 import { Jetton } from './Jetton'
 
-export type Currency = AgnosticNativeCurrency | Jetton
 export class Token extends Jetton {}
+export type Currency = AgnosticNativeCurrency | Token
