917e3a15d1f55e958aa34118509773755c1469a2
memoyil <2213635+memoyil@users.noreply.github.com>
2025-01-16 09:05:50 +0100

    chore: Comment out zkevm rpc

--- apps/web/src/config/nodes.ts
+++ apps/web/src/config/nodes.ts
@@ -121,7 +121,7 @@ export const PUBLIC_NODES: Record<ChainId, string[] | readonly string[]> = {
   ].filter(notEmpty),
   [ChainId.ARBITRUM_GOERLI]: arbitrumGoerli.rpcUrls.default.http,
   [ChainId.POLYGON_ZKEVM]: [
-    process.env.NEXT_PUBLIC_NODIES_POLYGON_ZKEVM || '',
+    // process.env.NEXT_PUBLIC_NODIES_POLYGON_ZKEVM || '',
     ...polygonZkEvm.rpcUrls.default.http,
     'https://f2562de09abc5efbd21eefa083ff5326.zkevm-rpc.com/',
     getGroveUrl(ChainId.POLYGON_ZKEVM, process.env.NEXT_PUBLIC_GROVE_API_KEY) || '',
