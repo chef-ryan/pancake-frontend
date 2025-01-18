fd2b018e17c914d9b79f0e8040fc19f47fea3ead
memoyil <2213635+memoyil@users.noreply.github.com>
2025-01-18 08:37:05 +0100

    perf: Use biome

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -50,6 +50,11 @@ const config = {
   typescript: {
     tsconfigPath: 'tsconfig.json',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   compiler: {
     styledComponents: true,
   },
