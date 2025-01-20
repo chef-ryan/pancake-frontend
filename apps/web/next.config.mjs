777ba5d3481829463b006ceaf56e23cad5aa33e9
memoyil <2213635+memoyil@users.noreply.github.com>
2025-01-20 10:41:56 +0100

    perf: Remove query core transpilation

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -76,9 +76,7 @@ const config = {
     '@pancakeswap/utils',
     '@pancakeswap/widgets-internal',
     '@pancakeswap/ifos',
-    '@pancakeswap/uikit',
-    // https://github.com/TanStack/query/issues/6560#issuecomment-1975771676
-    '@tanstack/query-core',
+    '@pancakeswap/uikit'
   ],
   reactStrictMode: true,
   swcMinify: false,
