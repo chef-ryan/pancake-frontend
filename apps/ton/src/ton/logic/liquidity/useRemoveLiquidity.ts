05406e27a68c7fc5713228f75b2ece29523d4da4
Chef Penguin <penguin@pancakeswap.com>
2025-02-13 09:51:56 +0530

    fix: native in remove liq

--- apps/ton/src/ton/logic/liquidity/useRemoveLiquidity.ts
+++ apps/ton/src/ton/logic/liquidity/useRemoveLiquidity.ts
@@ -1,12 +1,11 @@
 import { Currency } from '@pancakeswap/ton-v2-sdk'
 import { beginCell, toNano } from '@ton/core'
 import { SendTransactionRequest, useTonConnectUI } from '@tonconnect/ui-react'
+import { useUserAddress } from 'hooks/useUserAddress'
 import { useAtomValue } from 'jotai'
 import { useCallback } from 'react'
-import { addressAtom } from 'ton/atom/addressAtom'
 import { poolContractAtom } from 'ton/atom/contracts/poolContractAtom'
 import { poolAddressAtom } from 'ton/atom/liquidity/poolAddressAtom'
-import { parseAddress } from 'ton/utils/address'
 import { storeTokenBurn } from 'ton/wrappers/tact_LpWallet'
 
 interface RemoveLiquidityProps {
@@ -16,13 +15,13 @@ interface RemoveLiquidityProps {
 
 export const useRemoveLiquidity = ({ currency0, currency1 }: RemoveLiquidityProps) => {
   const [tonUI] = useTonConnectUI()
-  const userAddress_ = useAtomValue(addressAtom)
+  const userAddress = useUserAddress()
 
-  // TODO: Handle Native
+  // TODO: Check Native handling
   const poolAddress = useAtomValue(
     poolAddressAtom({
-      token0Address: currency0?.wrapped.address.toString(),
-      token1Address: currency1?.wrapped.address.toString(),
+      token0Address: currency0?.isNative ? userAddress.toString() : currency0?.address.toString(),
+      token1Address: currency1?.isNative ? userAddress.toString() : currency1?.address.toString(),
     }),
   )
 
@@ -30,8 +29,6 @@ export const useRemoveLiquidity = ({ currency0, currency1 }: RemoveLiquidityProp
 
   const removeLiquidity = useCallback(
     async (amount: bigint) => {
-      const userAddress = parseAddress(userAddress_)
-
       const userLpWallet = await poolContract.getGetWalletAddress(userAddress)
 
       const payload = beginCell()
@@ -59,7 +56,7 @@ export const useRemoveLiquidity = ({ currency0, currency1 }: RemoveLiquidityProp
 
       tonUI.sendTransaction(txRequest)
     },
-    [tonUI, userAddress_, poolContract, amount],
+    [tonUI, userAddress, poolContract],
   )
 
   return { removeLiquidity }
