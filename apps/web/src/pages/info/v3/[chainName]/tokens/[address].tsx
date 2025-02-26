c66ee72c755aff2612a46774df0d4ba016640c92
chef-ryan <ryan@pancakeswap.com>
2025-02-26 09:17:35 +0800

    remove get static props

--- apps/web/src/pages/info/v3/[chainName]/tokens/[address].tsx
+++ apps/web/src/pages/info/v3/[chainName]/tokens/[address].tsx
@@ -1,25 +1,22 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
 import dynamic from 'next/dynamic'
-import { getTokenStaticPaths, getTokenStaticProps } from 'utils/pageUtils'
+import { useRouter } from 'next/router'
 
 const InfoPageLayout = dynamic(() => import('views/V3Info/components/Layout').then((mod) => mod.InfoPageLayout), {
   ssr: false,
 })
 const Token = dynamic(() => import('views/V3Info/views/TokenPage'), { ssr: false })
 
-const TokenPage = ({ address }: { address: string }) => {
+const TokenPage = () => {
+  const router = useRouter()
+  const { address } = router.query
   if (!address) {
     return null
   }
 
-  return <Token address={address.toLowerCase()} />
+  return <Token address={String(address).toLowerCase()} />
 }
 
 TokenPage.Layout = InfoPageLayout
 TokenPage.chains = [] // set all
 
 export default TokenPage
-
-export const getStaticPaths: GetStaticPaths = getTokenStaticPaths()
-
-export const getStaticProps: GetStaticProps = getTokenStaticProps()
