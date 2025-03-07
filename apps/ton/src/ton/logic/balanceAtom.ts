d0f2f60adc54f4489d437e53cb86183359c97bd7
ChefEric <173023571+chef-eric@users.noreply.github.com>
2025-03-07 22:29:54 +0800

    fix: update balance after tx applied (#11305)
    <!-- start pr-codex -->
    
    ## PR-Codex overview
    This PR focuses on updating the `balanceAtom` in the
    `apps/ton/src/ton/logic/balanceAtom.ts` file to replace the dependency
    on `blockNumberAtom` with `txReceiptAtom`. This change is likely aimed
    at improving how transaction receipts are handled in relation to balance
    queries.
    
    ### Detailed summary
    - Deleted the file `apps/ton/src/hooks/useBlockNumber.ts`.
    - In `apps/ton/src/ton/logic/balanceAtom.ts`:
      - Removed the import of `blockNumberAtom`.
      - Added the import of `txReceiptAtom`.
    - Updated the `queryKey` in the `atomWithQuery` to use `txReceiptAtom`
    instead of `blockNumberAtom`.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/ton/src/ton/logic/balanceAtom.ts
+++ apps/ton/src/ton/logic/balanceAtom.ts
@@ -1,6 +1,6 @@
 import { Currency } from '@pancakeswap/ton-v2-sdk'
 import { Address } from '@ton/ton'
-import { blockNumberAtom } from 'hooks/useBlockNumber'
+import { txReceiptAtom } from 'hooks/useLatestTxReceipt'
 import { atomWithQuery } from 'jotai-tanstack-query'
 import { atomFamily } from 'jotai/utils'
 import isEqual from 'lodash/isEqual'
@@ -14,7 +14,7 @@ import { JettonWalletUSDT } from 'ton/wrappers/tact_JettonWalletUSDT'
 export const balanceAtom = atomFamily(
   (token?: Currency | null) =>
     atomWithQuery((get) => ({
-      queryKey: ['balance', get(networkAtom), get(addressAtom), token, get(blockNumberAtom)],
+      queryKey: ['balance', get(networkAtom), get(addressAtom), token, get(txReceiptAtom)],
       queryFn: async () => {
         if (!token) return 0n
         const client = TonContext.instance.getClient()
