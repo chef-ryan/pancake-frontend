577e9642e274e27114525e5b41acf7c30de12c48
chef-ryan <ryan@pancakeswap.com>
2025-03-10 21:44:56 +0800

    remove dynamic import

--- apps/web/src/components/AdPanel/index.ts
+++ apps/web/src/components/AdPanel/index.ts
@@ -1,14 +1,7 @@
-import dynamic from 'next/dynamic'
-
-// Dynamically import components
-const BodyText = dynamic(() => import('./BodyText').then((mod) => mod.BodyText))
-const Button = dynamic(() => import('./Button').then((mod) => mod.AdButton)) // Ensure to reference AdButton correctly
-const Card = dynamic(() => import('./Card').then((mod) => mod.AdCard)) // Ensure to reference AdCard correctly
-const AdPlayer = dynamic(() => import('./CardLayouts').then((mod) => mod.AdPlayer))
-const DesktopCard = dynamic(() => import('./CardLayouts').then((mod) => mod.DesktopCard))
-const MobileCard = dynamic(() => import('./CardLayouts').then((mod) => mod.MobileCard))
-const AdsSlides = dynamic(() => import('./CardLayouts').then((mod) => mod.AdSlides))
-const PicksAdsSlides = dynamic(() => import('./CardLayouts').then((mod) => mod.PickAdSlides))
+import { BodyText } from './BodyText'
+import { AdButton as Button } from './Button'
+import { AdCard as Card } from './Card'
+import { AdPlayer, DesktopCard, MobileCard, AdSlides as AdsSlides, PickAdSlides as PicksAdsSlides } from './CardLayouts'
 
 export const AdPanel = {
   DesktopCard,
