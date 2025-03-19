6b7bb5215a68609e9c384df54208d4cfe80e6f4e
chef-ryan <ryan@pancakeswap.com>
2025-03-19 13:26:53 +0800

    fix monad testnet issues

--- packages/tokens/src/constants/monadTestnet.ts
+++ packages/tokens/src/constants/monadTestnet.ts
@@ -1,9 +1,10 @@
 import { ChainId } from '@pancakeswap/chains'
 import { WETH9 } from '@pancakeswap/sdk'
-import { BUSD, USDC } from './common'
+import { BUSD, USDC, USDT } from './common'
 
 export const monadTestnetTokens = {
   weth: WETH9[ChainId.MONAD_TESTNET],
   busd: BUSD[ChainId.MONAD_TESTNET],
   usdc: USDC[ChainId.MONAD_TESTNET],
+  usdt: USDT[ChainId.MONAD_TESTNET],
 }
