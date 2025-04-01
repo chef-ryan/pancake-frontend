392ccc4a43263bfa5cec4592614761002843b5bf
memoyil <2213635+memoyil@users.noreply.github.com>
2025-04-01 19:03:26 +0200

    feat: Show latest ido when no query

--- apps/web/src/views/Idos/hooks/ido/useCurrentIDOConfig.ts
+++ apps/web/src/views/Idos/hooks/ido/useCurrentIDOConfig.ts
@@ -4,5 +4,9 @@ import { IDOConfig, idoConfigDict } from '../../config'
 export const useCurrentIDOConfig = (): IDOConfig | undefined => {
   const { query } = useRouter()
   const currentIdo = query.ido as string
-  return idoConfigDict[currentIdo]
+  if (currentIdo) {
+    return idoConfigDict[currentIdo]
+  }
+  const idoEntries = Object.entries(idoConfigDict)
+  return idoEntries.length > 0 ? idoEntries[idoEntries.length - 1][1] : undefined
 }
