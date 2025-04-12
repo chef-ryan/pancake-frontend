4eb0ea7cd4b18dacd390a1125191d7b75960dc00
chef-ryan <ryan@pancakeswap.com>
2025-04-12 23:46:20 +0800

    save

--- apps/web/src/hooks/useStablecoinPrice.ts
+++ apps/web/src/hooks/useStablecoinPrice.ts
@@ -1,14 +1,10 @@
 import { ChainId } from '@pancakeswap/chains'
-import { Currency, CurrencyAmount, Price, TradeType } from '@pancakeswap/sdk'
-import { SmartRouterTrade } from '@pancakeswap/smart-router'
+import { Currency, CurrencyAmount, Price } from '@pancakeswap/sdk'
 import { CAKE, STABLE_COIN } from '@pancakeswap/tokens'
 import { getFullDecimalMultiplier } from '@pancakeswap/utils/getFullDecimalMultiplier'
 import { useCakePrice } from 'hooks/useCakePrice'
 import { useMemo } from 'react'
-import { warningSeverity } from 'utils/exchange'
 import { multiplyPriceByAmount } from 'utils/prices'
-import { computeTradePriceBreakdown } from 'views/Swap/V3Swap/utils/exchange'
-import { useBestAMMTrade } from './quoter/useBestAMMTrade'
 import { useActiveChainId } from './useActiveChainId'
 import { useCurrencyUsdPrice } from './useCurrencyUsdPrice'
 
@@ -51,16 +47,16 @@ export function useStablecoinPrice(
     [stableCoin],
   )
 
-  const { trade } = useBestAMMTrade({
-    amount: amountOut,
-    currency: currency ?? undefined,
-    baseCurrency: stableCoin,
-    tradeType: TradeType.EXACT_OUTPUT,
-    maxSplits: 0,
-    enabled: Boolean(!isLoading && !priceFromApi && shouldEnabled),
-    autoRevalidate: false,
-    type: 'quoter',
-  })
+  // const { trade } = useBestAMMTrade({
+  //   amount: amountOut,
+  //   currency: currency ?? undefined,
+  //   baseCurrency: stableCoin,
+  //   tradeType: TradeType.EXACT_OUTPUT,
+  //   maxSplits: 0,
+  //   enabled: Boolean(!isLoading && !priceFromApi && shouldEnabled),
+  //   autoRevalidate: false,
+  //   type: 'quoter',
+  // })
 
   const price = useMemo(() => {
     if (!currency || !stableCoin || !enabled) {
@@ -90,23 +86,23 @@ export function useStablecoinPrice(
       )
     }
 
-    if (trade) {
-      const { inputAmount, outputAmount } = trade as unknown as SmartRouterTrade<TradeType>
+    // if (trade) {
+    //   const { inputAmount, outputAmount } = trade as unknown as SmartRouterTrade<TradeType>
 
-      // if price impact is too high, don't show price
-      if (hideIfPriceImpactTooHigh) {
-        const { priceImpactWithoutFee } = computeTradePriceBreakdown(trade as unknown as SmartRouterTrade<TradeType>)
+    //   // if price impact is too high, don't show price
+    //   if (hideIfPriceImpactTooHigh) {
+    //     const { priceImpactWithoutFee } = computeTradePriceBreakdown(trade as unknown as SmartRouterTrade<TradeType>)
 
-        if (!priceImpactWithoutFee || warningSeverity(priceImpactWithoutFee) > 2) {
-          return undefined
-        }
-      }
+    //     if (!priceImpactWithoutFee || warningSeverity(priceImpactWithoutFee) > 2) {
+    //       return undefined
+    //     }
+    //   }
 
-      return new Price(currency, stableCoin, inputAmount.quotient, outputAmount.quotient)
-    }
+    //   return new Price(currency, stableCoin, inputAmount.quotient, outputAmount.quotient)
+    // }
 
     return undefined
-  }, [currency, stableCoin, enabled, isCake, cakePrice, isStableCoin, priceFromApi, trade, hideIfPriceImpactTooHigh])
+  }, [currency, stableCoin, enabled, isCake, cakePrice, isStableCoin, priceFromApi, hideIfPriceImpactTooHigh])
 
   if (price?.denominator === 0n) {
     return undefined
