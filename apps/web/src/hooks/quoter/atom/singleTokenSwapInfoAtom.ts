167a96fa24ab68507bf5fa473f2ab2bf6c4a41cd
chef-ryan <ryan@pancakeswap.com>
2025-04-12 17:40:58 +0800

    save

--- apps/web/src/hooks/quoter/atom/singleTokenSwapInfoAtom.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'jotai'
-import { useSingleTokenSwapInfo } from 'state/swap/hooks'
-
-type SwapInfo = ReturnType<typeof useSingleTokenSwapInfo>
-export const singleTokenSwapInfoAtom = atom<SwapInfo>({})
