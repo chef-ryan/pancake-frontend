91fddd3baeca8bf88b16c40dafb04b64c6a2fddb
Chef Jerry <jerry@pancakeswap.com>
2025-04-15 14:23:31 +0800

    chore: apply test env endpoints for test

--- packages/stable-swap-sdk/src/config/endpoint.ts
+++ packages/stable-swap-sdk/src/config/endpoint.ts
@@ -1,1 +1,1 @@
-export const STABLE_SWAP_API = 'https://configs.pancakeswap.com/api/data/cached/stableswaps'
+export const STABLE_SWAP_API = 'https://cms-public-api-7ys4p.ondigitalocean.app/api/data/cached/stableswaps'
