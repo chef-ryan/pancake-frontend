f27ca58301903c0da0177f9c0ebd5473b114a5be
chef-ryan <ryan@pancakeswap.com>
2025-06-04 00:20:26 +0800

    code

--- apps/web/src/views/LimitOrders/components/LimitOrderTable/LoadingTable.tsx
+++ apps/web/src/views/LimitOrders/components/LimitOrderTable/LoadingTable.tsx
@@ -1,30 +1,44 @@
-import { Skeleton, Table, Td } from '@pancakeswap/uikit'
 import { memo } from 'react'
-import styled from 'styled-components'
+import { Skeleton, Table, Td } from '@pancakeswap/uikit'
 
-const LoadingTable = ({ lines, className }: { lines?: number; className?: string }) => (
-  <Table className={className}>
+const LoadingTable = () => (
+  <Table>
     <tbody>
-      {Array.from({ length: lines || 3 }).map((_, index) => (
-        <tr key={index}>
-          <Td>
-            <StyledSkeleton />
-          </Td>
-          <Td>
-            <StyledSkeleton />
-          </Td>
-          <Td>
-            <StyledSkeleton />
-          </Td>
-        </tr>
-      ))}
+      <tr>
+        <Td>
+          <Skeleton />
+        </Td>
+        <Td>
+          <Skeleton />
+        </Td>
+        <Td>
+          <Skeleton />
+        </Td>
+      </tr>
+      <tr>
+        <Td>
+          <Skeleton />
+        </Td>
+        <Td>
+          <Skeleton />
+        </Td>
+        <Td>
+          <Skeleton />
+        </Td>
+      </tr>
+      <tr>
+        <Td>
+          <Skeleton />
+        </Td>
+        <Td>
+          <Skeleton />
+        </Td>
+        <Td>
+          <Skeleton />
+        </Td>
+      </tr>
     </tbody>
   </Table>
 )
-
-const StyledSkeleton = styled(Skeleton)`
-  background: ${({ theme }) => theme.colors.bubblegum};
-  opacity: 1;
-`
 
 export default memo(LoadingTable)
