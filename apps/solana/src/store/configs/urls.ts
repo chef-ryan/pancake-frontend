b6422b5a6f577208febac11bd2fa12444de6b6bc
ChefEric <173023571+chef-eric@users.noreply.github.com>
2025-06-11 11:09:46 +0800

    fix: update pool search api

--- apps/solana/src/store/configs/urls.ts
+++ apps/solana/src/store/configs/urls.ts
@@ -4,7 +4,7 @@ export const urlConfigs = {
   ...API_URLS,
   BASE_HOST: process.env.NEXT_PUBLIC_EXPLORE_API_ENDPOINT ?? API_URLS.BASE_HOST,
   POOL_LIST: '/cached/v1/pools/info/list',
-  POOL_SEARCH_MINT: '/cached/v1/pools/info/ids',
   MINT_PRICE: '/cached/v1/tokens/price',
-  INFO: '/cached/v1/pools/stats/overview'
+  INFO: '/cached/v1/pools/stats/overview',
+  POOL_SEARCH_BY_ID: '/cached/v1/pools/info/ids'
 }
