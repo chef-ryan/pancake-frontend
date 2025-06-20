e24e0bd443cf9a4057b876adff37f9ee6f3e437a
chef-ryan <ryan@pancakeswap.com>
2025-06-20 18:06:30 +0800

    optimize v2 token page display

--- apps/web/src/views/Info/Tokens/TokenPage.tsx
+++ apps/web/src/views/Info/Tokens/TokenPage.tsx
@@ -10,7 +10,7 @@ import {
   Heading,
   Image,
   ScanLink,
-  Spinner,
+  Skeleton,
   Text,
   Link as UIKitLink,
   useMatchBreakpoints,
@@ -119,160 +119,143 @@ const TokenPage: React.FC<React.PropsWithChildren<{ routeAddress: string }>> = (
   return (
     <Page>
       <NextSeo title={tokenData?.symbol} />
-      {tokenData ? (
-        !tokenData.exists ? (
-          <Card>
-            <Box p="16px">
-              <Text>
-                {t('No pair has been created with this token yet. Create one')}
-                <NextLinkFromReactRouter style={{ display: 'inline', marginLeft: '6px' }} to={`/add/${address}`}>
-                  {t('here.')}
-                </NextLinkFromReactRouter>
-              </Text>
-            </Box>
-          </Card>
-        ) : (
-          <>
-            {/* Stuff on top */}
-            <Flex justifyContent="space-between" mb="24px" flexDirection={['column', 'column', 'row']}>
-              <Breadcrumbs mb="32px">
-                <NextLinkFromReactRouter to={`/info${chainPath}${infoTypeParam}`}>
-                  <Text color="primary">{t('Info')}</Text>
-                </NextLinkFromReactRouter>
-                <NextLinkFromReactRouter to={`/info${chainPath}/tokens${infoTypeParam}`}>
-                  <Text color="primary">{t('Tokens')}</Text>
-                </NextLinkFromReactRouter>
-                <Flex>
-                  <Text mr="8px">{tokenSymbol}</Text>
-                  <Text>{`(${truncateHash(address)})`}</Text>
-                </Flex>
-              </Breadcrumbs>
-              <Flex justifyContent={[null, null, 'flex-end']} mt={['8px', '8px', 0]} alignItems="center">
-                <ScanLink
-                  mr="8px"
-                  color="primary"
-                  useBscCoinFallback={ChainLinkSupportChains.includes(multiChainId[chainName])}
-                  href={getBlockExploreLink(address, 'address', multiChainId[chainName])}
-                >
-                  {t('View on %site%', { site: multiChainScan[chainName] })}
-                </ScanLink>
-                {cmcLink && (
-                  <StyledCMCLink
-                    href={cmcLink}
-                    rel="noopener noreferrer nofollow"
-                    target="_blank"
-                    title="CoinMarketCap"
-                  >
-                    <Image src="/images/CMC-logo.svg" height={22} width={22} alt={t('View token on CoinMarketCap')} />
-                  </StyledCMCLink>
-                )}
-                {/* <SaveIcon
+      <>
+        {/* Stuff on top */}
+        <Flex justifyContent="space-between" mb="24px" flexDirection={['column', 'column', 'row']}>
+          <Breadcrumbs mb="32px">
+            <NextLinkFromReactRouter to={`/info${chainPath}${infoTypeParam}`}>
+              <Text color="primary">{t('Info')}</Text>
+            </NextLinkFromReactRouter>
+            <NextLinkFromReactRouter to={`/info${chainPath}/tokens${infoTypeParam}`}>
+              <Text color="primary">{t('Tokens')}</Text>
+            </NextLinkFromReactRouter>
+            <Flex>
+              <Text mr="8px">{tokenSymbol}</Text>
+              <Text>{`(${truncateHash(address)})`}</Text>
+            </Flex>
+          </Breadcrumbs>
+          <Flex justifyContent={[null, null, 'flex-end']} mt={['8px', '8px', 0]} alignItems="center">
+            <ScanLink
+              mr="8px"
+              color="primary"
+              useBscCoinFallback={ChainLinkSupportChains.includes(multiChainId[chainName])}
+              href={getBlockExploreLink(address, 'address', multiChainId[chainName])}
+            >
+              {t('View on %site%', { site: multiChainScan[chainName] })}
+            </ScanLink>
+            {cmcLink && (
+              <StyledCMCLink href={cmcLink} rel="noopener noreferrer nofollow" target="_blank" title="CoinMarketCap">
+                <Image src="/images/CMC-logo.svg" height={22} width={22} alt={t('View token on CoinMarketCap')} />
+              </StyledCMCLink>
+            )}
+            {/* <SaveIcon
                   fill={savedTokens.includes(address)}
                   onClick={() => (savedTokens.includes(address) ? removeToken(address) : addToken(address))}
                 /> */}
-                <CopyButton ml="4px" text={address} tooltipMessage={t('Token address copied')} />
-              </Flex>
-            </Flex>
-            <Flex justifyContent="space-between" flexDirection={['column', 'column', 'column', 'row']}>
-              <Flex flexDirection="column" mb={['8px', null]}>
-                <Flex alignItems="center">
-                  <CurrencyLogo size="32px" address={address} chainName={chainName} />
-                  <Text
-                    ml="12px"
-                    bold
-                    lineHeight="0.7"
-                    fontSize={isXs || isSm ? '24px' : '40px'}
-                    id="info-token-name-title"
-                  >
-                    {tokenName}
-                  </Text>
-                  <Text ml="12px" lineHeight="1" color="textSubtle" fontSize={isXs || isSm ? '14px' : '20px'}>
-                    ({tokenSymbol})
-                  </Text>
-                </Flex>
-                <Flex mt="8px" ml="46px" alignItems="center">
-                  <Text mr="16px" bold fontSize="24px">
-                    ${formatAmount(tokenData.priceUSD, { notation: 'standard' })}
-                  </Text>
-                  <Percent value={tokenData.priceUSDChange} fontWeight={600} />
-                </Flex>
-              </Flex>
-              <Flex>
-                <NextLinkFromReactRouter to={`/add/${address}?chain=${CHAIN_QUERY_NAME[chainId]}`}>
-                  <Button mr="8px" variant="secondary">
-                    {t('Add Liquidity')}
-                  </Button>
-                </NextLinkFromReactRouter>
-                <NextLinkFromReactRouter to={`/swap?outputCurrency=${address}&chainId=${multiChainId[chainName]}`}>
-                  <Button>{t('Trade')}</Button>
-                </NextLinkFromReactRouter>
-              </Flex>
-            </Flex>
-
-            {/* data on the right side of chart */}
-            <ContentLayout>
-              <Card>
-                <Box p="24px">
-                  <Text bold small color="secondary" fontSize="12px" textTransform="uppercase">
-                    {t('Liquidity')}
-                  </Text>
-                  <Text bold fontSize="24px">
-                    ${formatAmount(tokenData.liquidityUSD)}
-                  </Text>
-                  <Percent value={tokenData.liquidityUSDChange} />
-
-                  <Text mt="24px" bold color="secondary" fontSize="12px" textTransform="uppercase">
-                    {t('Volume 24H')}
-                  </Text>
-                  <Text bold fontSize="24px" textTransform="uppercase">
-                    ${formatAmount(tokenData.volumeUSD)}
-                  </Text>
-                  <Percent value={tokenData.volumeUSDChange} />
-
-                  <Text mt="24px" bold color="secondary" fontSize="12px" textTransform="uppercase">
-                    {t('Volume 7D')}
-                  </Text>
-                  <Text bold fontSize="24px">
-                    ${formatAmount(tokenData.volumeUSDWeek)}
-                  </Text>
-
-                  <Text mt="24px" bold color="secondary" fontSize="12px" textTransform="uppercase">
-                    {t('Transactions 24H')}
-                  </Text>
-                  <Text bold fontSize="24px">
-                    {formatAmount(tokenData.txCount, { isInteger: true })}
-                  </Text>
-                </Box>
-              </Card>
-              {/* charts card */}
-              <ChartCard
-                variant="token"
-                volumeChartData={volumeChartData}
-                tvlChartData={tvlChartData}
-                tokenData={tokenData}
-                tokenPriceData={priceData}
-              />
-            </ContentLayout>
-
-            {/* pools and transaction tables */}
-            <Heading scale="lg" mb="16px" mt="40px">
-              {t('Pairs')}
-            </Heading>
-
-            <PoolTable poolDatas={poolDatas} />
-
-            <Heading scale="lg" mb="16px" mt="40px">
-              {t('Transactions')}
-            </Heading>
-
-            <TransactionTable transactions={transactions} />
-          </>
-        )
-      ) : (
-        <Flex mt="80px" justifyContent="center">
-          <Spinner />
+            <CopyButton ml="4px" text={address} tooltipMessage={t('Token address copied')} />
+          </Flex>
         </Flex>
-      )}
+        <Flex justifyContent="space-between" flexDirection={['column', 'column', 'column', 'row']}>
+          <Flex flexDirection="column" mb={['8px', null]}>
+            <Flex alignItems="center">
+              <CurrencyLogo size="32px" address={address} chainName={chainName} />
+              <Text
+                ml="12px"
+                bold
+                lineHeight="0.7"
+                fontSize={isXs || isSm ? '24px' : '40px'}
+                id="info-token-name-title"
+              >
+                {tokenName}
+              </Text>
+              <Text ml="12px" lineHeight="1" color="textSubtle" fontSize={isXs || isSm ? '14px' : '20px'}>
+                ({tokenSymbol})
+              </Text>
+            </Flex>
+            <Flex mt="8px" ml="46px" alignItems="center">
+              <Text mr="16px" bold fontSize="24px">
+                ${formatAmount(tokenData?.priceUSD, { notation: 'standard' })}
+              </Text>
+              <Percent value={tokenData?.priceUSDChange} fontWeight={600} />
+            </Flex>
+          </Flex>
+          <Flex>
+            <NextLinkFromReactRouter to={`/add/${address}?chain=${CHAIN_QUERY_NAME[chainId]}`}>
+              <Button mr="8px" variant="secondary">
+                {t('Add Liquidity')}
+              </Button>
+            </NextLinkFromReactRouter>
+            <NextLinkFromReactRouter to={`/swap?outputCurrency=${address}&chainId=${multiChainId[chainName]}`}>
+              <Button>{t('Trade')}</Button>
+            </NextLinkFromReactRouter>
+          </Flex>
+        </Flex>
+
+        {/* data on the right side of chart */}
+        <ContentLayout>
+          {tokenData && (
+            <Card>
+              <Box p="24px">
+                <Text bold small color="secondary" fontSize="12px" textTransform="uppercase">
+                  {t('Liquidity')}
+                </Text>
+                <Text bold fontSize="24px">
+                  ${formatAmount(tokenData.liquidityUSD)}
+                </Text>
+                <Percent value={tokenData.liquidityUSDChange} />
+
+                <Text mt="24px" bold color="secondary" fontSize="12px" textTransform="uppercase">
+                  {t('Volume 24H')}
+                </Text>
+                <Text bold fontSize="24px" textTransform="uppercase">
+                  ${formatAmount(tokenData.volumeUSD)}
+                </Text>
+                <Percent value={tokenData.volumeUSDChange} />
+
+                <Text mt="24px" bold color="secondary" fontSize="12px" textTransform="uppercase">
+                  {t('Volume 7D')}
+                </Text>
+                <Text bold fontSize="24px">
+                  ${formatAmount(tokenData.volumeUSDWeek)}
+                </Text>
+
+                <Text mt="24px" bold color="secondary" fontSize="12px" textTransform="uppercase">
+                  {t('Transactions 24H')}
+                </Text>
+                <Text bold fontSize="24px">
+                  {formatAmount(tokenData.txCount, { isInteger: true })}
+                </Text>
+              </Box>
+            </Card>
+          )}
+          {!tokenData && (
+            <Card>
+              <Skeleton />
+            </Card>
+          )}
+          {/* charts card */}
+          <ChartCard
+            variant="token"
+            volumeChartData={volumeChartData}
+            tvlChartData={tvlChartData}
+            tokenData={tokenData}
+            tokenPriceData={priceData}
+          />
+        </ContentLayout>
+
+        {/* pools and transaction tables */}
+        <Heading scale="lg" mb="16px" mt="40px">
+          {t('Pairs')}
+        </Heading>
+
+        <PoolTable poolDatas={poolDatas} />
+
+        <Heading scale="lg" mb="16px" mt="40px">
+          {t('Transactions')}
+        </Heading>
+
+        <TransactionTable transactions={transactions} />
+      </>
     </Page>
   )
 }
