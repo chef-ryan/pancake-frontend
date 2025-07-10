be291f59eea9b5cfe31fb7413510229003fbb209
chef-ryan <ryan@pancakeswap.com>
2025-07-10 11:02:29 +0800

    update

--- apps/web/src/hooks/useAuth.tsx
+++ apps/web/src/hooks/useAuth.tsx
@@ -1,14 +1,12 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { WalletConnectorNotFoundError, WalletSwitchChainError } from '@pancakeswap/ui-wallets'
-import { CHAIN_QUERY_NAME } from 'config/chains'
 import { ConnectorNames } from 'config/wallet'
-import { useAtom } from 'jotai'
 import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 import { useAppDispatch } from 'state'
 import { ConnectorNotFoundError, SwitchChainNotSupportedError, useAccount, useConnect, useDisconnect } from 'wagmi'
 import { clearUserStates } from '../utils/clearUserStates'
-import { queryChainIdAtom, useActiveChainId } from './useActiveChainId'
+import { useActiveChainId } from './useActiveChainId'
 
 const useAuth = () => {
   const dispatch = useAppDispatch()
@@ -16,7 +14,6 @@ const useAuth = () => {
   const { chain } = useAccount()
   const { disconnectAsync } = useDisconnect()
   const { chainId } = useActiveChainId()
-  const [, setQueryChainId] = useAtom(queryChainIdAtom)
   const { t } = useTranslation()
   const router = useRouter()
 
@@ -25,26 +22,7 @@ const useAuth = () => {
       const findConnector = connectors.find((c) => c.id === connectorID)
       try {
         if (!findConnector) return undefined
-
-        const connected = await connectAsync({ connector: findConnector, chainId })
-        if (connected.chainId !== chainId) {
-          router.replace(
-            {
-              pathname: router.pathname,
-              query: {
-                ...router.query,
-                chain: CHAIN_QUERY_NAME[connected.chainId],
-              },
-            },
-            undefined,
-            {
-              shallow: true,
-            },
-          )
-
-          setQueryChainId(connected.chainId)
-        }
-        return connected
+        return await connectAsync({ connector: findConnector, chainId })
       } catch (error) {
         if (error instanceof ConnectorNotFoundError) {
           throw new WalletConnectorNotFoundError()
@@ -59,7 +37,7 @@ const useAuth = () => {
       }
       return undefined
     },
-    [connectors, connectAsync, chainId, setQueryChainId, t, router],
+    [connectors, connectAsync, chainId, t, router],
   )
 
   const logout = useCallback(async () => {
