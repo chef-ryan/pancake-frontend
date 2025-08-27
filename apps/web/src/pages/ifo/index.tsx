c20bf370f2e8ae2ef803a20fca7cc4e033b8d080
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-08-27 15:05:14 +0800

    chore: replace old ifo page and keep v1 to new router

--- apps/web/src/pages/ifo/index.tsx
+++ apps/web/src/pages/ifo/index.tsx
@@ -1,14 +1,22 @@
-import { SUPPORTED_CHAIN_IDS } from '@pancakeswap/ifos'
+// TODO: Using IfosV2 components for IFO v10 testing
+import { ChainId } from '@pancakeswap/chains'
+// import { SUPPORTED_CHAIN_IDS } from '@pancakeswap/ifos'
 
-import { IfoPageLayout } from '../../views/Ifos'
-import Ifo from '../../views/Ifos/Ifo'
+// import { IfoPageLayout } from '../../views/Ifos'
+// import Ifo from '../../views/Ifos/Ifo'
+import { IdoPageLayout } from '../../views/IfosV2'
+import IFO from '../../views/IfosV2/ido'
+
+const IFO_SUPPORT_CHAINS = [ChainId.BSC, ChainId.BSC_TESTNET]
 
 const CurrentIfoPage = () => {
-  return <Ifo />
+  return <IFO />
 }
 
-CurrentIfoPage.Layout = IfoPageLayout
+CurrentIfoPage.Layout = IdoPageLayout
 
-CurrentIfoPage.chains = SUPPORTED_CHAIN_IDS
+CurrentIfoPage.chains = IFO_SUPPORT_CHAINS
+
+CurrentIfoPage.mp = true
 
 export default CurrentIfoPage
