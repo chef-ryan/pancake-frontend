75de2d8f95402a257d50ddbff90b7ed8782d89e0
chef-ryan <ryan@pancakeswap.com>
2025-08-31 11:42:01 +0800

    fix types

--- apps/web/src/views/IfosV2/hooks/useIfo.ts
+++ apps/web/src/views/IfosV2/hooks/useIfo.ts
@@ -8,14 +8,16 @@ const useIfo = () => {
   const info = useIFOInfo()
   const { stakeCurrency0, stakeCurrency1 } = useIFOCurrencies()
 
-  const pools = useMemo<IfoPool[]>(
-    () =>
-      [
-        { currency: stakeCurrency0, price: info.pricePerTokens[0], raise: info.raiseAmounts[0] },
-        { currency: stakeCurrency1, price: info.pricePerTokens[1], raise: info.raiseAmounts[1] },
-      ].filter((p): p is IfoPool => Boolean(p.currency)),
-    [stakeCurrency0, stakeCurrency1, info.pricePerTokens, info.raiseAmounts],
-  )
+  const pools = useMemo<IfoPool[]>(() => {
+    const list: IfoPool[] = []
+    if (stakeCurrency0) {
+      list.push({ currency: stakeCurrency0, price: info.pricePerTokens[0], raise: info.raiseAmounts[0] })
+    }
+    if (stakeCurrency1) {
+      list.push({ currency: stakeCurrency1, price: info.pricePerTokens[1], raise: info.raiseAmounts[1] })
+    }
+    return list
+  }, [stakeCurrency0, stakeCurrency1, info.pricePerTokens, info.raiseAmounts])
 
   return { ...ctx, info, pools }
 }
