6bdda4bf957963bd0b9a5196d5ceca401c9af32f
chef-ryan <ryan@pancakeswap.com>
2025-09-02 17:28:01 +0800

    update deposit form

--- apps/web/src/views/IfosV2/components/IfoDeposit.tsx
+++ apps/web/src/views/IfosV2/components/IfoDeposit.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { FlexGap, InfoIcon, Text, useTooltip } from '@pancakeswap/uikit'
+import { Box, Card, CardBody, CardHeader, FlexGap, InfoIcon, Text, useTooltip } from '@pancakeswap/uikit'
+import { styled } from 'styled-components'
 import ConnectW3WButton from 'components/ConnectW3WButton'
 import { logGTMIfoConnectWalletEvent } from 'utils/customGTMEventTracking'
 import { useAccount } from 'wagmi'
@@ -7,11 +8,34 @@ import { CurrencyLogo } from '@pancakeswap/widgets-internal'
 import { Divider } from './IfoCards/Divider'
 import { IfoDepositForm } from './IfoCards/IfoDepositForm'
 import { StakedDisplay } from './IfoCards/StakedDisplay'
+import { IfoRibbon } from './IfoCards/IfoRibbon'
 import { useIFOCurrencies } from '../hooks/ifo/useIFOCurrencies'
 import { useIFOStatus } from '../hooks/ifo/useIFOStatus'
 import { useIFOUserStatus } from '../hooks/ifo/useIFOUserStatus'
 import useIfo from '../hooks/useIfo'
 import { useIfoDisplay } from '../hooks/useIfoDisplay'
+
+const Header = styled(CardHeader)<{ $bannerUrl: string }>`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 64px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: ${({ theme }) => theme.colors.dropdown};
+  background-image: ${({ $bannerUrl }) => `url('${$bannerUrl}')`};
+`
+
+const StyledCard = styled(Card)`
+  width: 100%;
+  margin: 0 auto;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    width: 737px;
+  }
+`
 
 export const IfoDeposit: React.FC<{ pid: number }> = ({ pid }) => {
   const { t } = useTranslation()
@@ -24,12 +48,13 @@ export const IfoDeposit: React.FC<{ pid: number }> = ({ pid }) => {
   const ifoStatus = pid === 0 ? ifoStatus0 : ifoStatus1
   const userHasStaked = userStatus?.stakedAmount?.greaterThan(0)
 
-  const { info, pools } = useIfo()
+  const { info, pools, config } = useIfo()
   const { pools: displayPools } = useIfoDisplay()
   const { status } = info
   const poolInfo = pools?.[pid]
   const raiseAmountText = displayPools?.[pid]?.raiseAmountText
   const pricePerToken = poolInfo?.price
+  const bannerUrl = config?.bannerUrl ?? ''
 
   const { targetRef, tooltip, tooltipVisible } = useTooltip(
     t('This sale has been oversubscribed. You will get partial refund of the deposit.'),
@@ -47,68 +72,76 @@ export const IfoDeposit: React.FC<{ pid: number }> = ({ pid }) => {
   }
 
   return (
-    <FlexGap flexDirection="column" gap="8px">
-      {userHasStaked ? (
-        <StakedDisplay userStatus={userStatus} pid={pid} />
-      ) : (
-        <FlexGap flexDirection="column" gap="8px">
-          <FlexGap alignItems="center" gap="4px">
-            <CurrencyLogo currency={stakeCurrency} size="24px" />
-            <Text fontSize="12px" bold color="secondary" lineHeight="18px" textTransform="uppercase">
-              {stakeCurrency?.symbol} {t('Pool')}
-            </Text>
-          </FlexGap>
-          {account ? (
-            <IfoDepositForm userStatus={userStatus} pid={pid} />
-          ) : (
-            <ConnectW3WButton width="100%" onClick={handleConnectWallet} />
-          )}
-        </FlexGap>
-      )}
-
-      {userHasStaked && <Divider />}
-      <FlexGap justifyContent="space-between" mt="8px">
-        <Text color="textSubtle">
-          {t('Sale Price per')} {offeringCurrency?.symbol ?? ''}
-        </Text>
-        <Text>
-          {pricePerToken?.toSignificant(6)} {stakeCurrency?.symbol ?? ''}
-        </Text>
-      </FlexGap>
-      <FlexGap justifyContent="space-between">
-        <Text color="textSubtle">{t('Target Raise')}</Text>
-        <Text>{raiseAmountText}</Text>
-      </FlexGap>
-      {status === 'live' && (
-        <>
-          <FlexGap justifyContent="space-between">
-            <Text color="textSubtle">{t('Total committed')}</Text>
-            <Text>
-              {ifoStatus.currentStakedAmount?.toSignificant(6) ?? 0} {stakeCurrency?.symbol ?? ''}
-            </Text>
-          </FlexGap>
-          <FlexGap justifyContent="space-between">
-            <Text color="textSubtle">{t('Status')}</Text>
-            <FlexGap flexDirection="column" alignItems="flex-end">
-              <FlexGap gap="3px">
-                <Text>
-                  {ifoStatus.progress.toFixed(2)} % {ifoStatus.progress.greaterThan(1) && '🎉'}
-                </Text>
+    <StyledCard>
+      <Box className="sticky-header" position="sticky" bottom="48px" width="100%" zIndex={6}>
+        <Header $bannerUrl={bannerUrl} />
+        <IfoRibbon />
+        <CardBody>
+          <FlexGap flexDirection="column" gap="8px">
+            {userHasStaked ? (
+              <StakedDisplay userStatus={userStatus} pid={pid} />
+            ) : (
+              <FlexGap flexDirection="column" gap="8px">
+                <FlexGap alignItems="center" gap="4px">
+                  <CurrencyLogo currency={stakeCurrency} size="24px" />
+                  <Text fontSize="12px" bold color="secondary" lineHeight="18px" textTransform="uppercase">
+                    {stakeCurrency?.symbol} {t('Pool')}
+                  </Text>
+                </FlexGap>
+                {account ? (
+                  <IfoDepositForm userStatus={userStatus} pid={pid} />
+                ) : (
+                  <ConnectW3WButton width="100%" onClick={handleConnectWallet} />
+                )}
               </FlexGap>
-              {ifoStatus.progress.greaterThan(1) && (
-                <FlexGap gap="3px">
-                  <Text>{t('Oversubscribed')}</Text>
-                  <FlexGap ref={targetRef}>
-                    <InfoIcon width="14px" color="textSubtle" />
-                    {tooltipVisible && tooltip}
+            )}
+
+            {userHasStaked && <Divider />}
+            <FlexGap justifyContent="space-between" mt="8px">
+              <Text color="textSubtle">
+                {t('Sale Price per')} {offeringCurrency?.symbol ?? ''}
+              </Text>
+              <Text>
+                {pricePerToken?.toSignificant(6)} {stakeCurrency?.symbol ?? ''}
+              </Text>
+            </FlexGap>
+            <FlexGap justifyContent="space-between">
+              <Text color="textSubtle">{t('Target Raise')}</Text>
+              <Text>{raiseAmountText}</Text>
+            </FlexGap>
+            {status === 'live' && (
+              <>
+                <FlexGap justifyContent="space-between">
+                  <Text color="textSubtle">{t('Total committed')}</Text>
+                  <Text>
+                    {ifoStatus.currentStakedAmount?.toSignificant(6) ?? 0} {stakeCurrency?.symbol ?? ''}
+                  </Text>
+                </FlexGap>
+                <FlexGap justifyContent="space-between">
+                  <Text color="textSubtle">{t('Status')}</Text>
+                  <FlexGap flexDirection="column" alignItems="flex-end">
+                    <FlexGap gap="3px">
+                      <Text>
+                        {ifoStatus.progress.toFixed(2)} % {ifoStatus.progress.greaterThan(1) && '🎉'}
+                      </Text>
+                    </FlexGap>
+                    {ifoStatus.progress.greaterThan(1) && (
+                      <FlexGap gap="3px">
+                        <Text>{t('Oversubscribed')}</Text>
+                        <FlexGap ref={targetRef}>
+                          <InfoIcon width="14px" color="textSubtle" />
+                          {tooltipVisible && tooltip}
+                        </FlexGap>
+                      </FlexGap>
+                    )}
                   </FlexGap>
                 </FlexGap>
-              )}
-            </FlexGap>
+              </>
+            )}
           </FlexGap>
-        </>
-      )}
-    </FlexGap>
+        </CardBody>
+      </Box>
+    </StyledCard>
   )
 }
 
