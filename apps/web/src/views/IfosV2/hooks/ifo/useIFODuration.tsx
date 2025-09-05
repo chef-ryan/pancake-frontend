502fe83ae1e8a31b5417c482ec2d251cb5a211b6
chef-ryan <ryan@pancakeswap.com>
2025-09-05 14:21:10 +0800

    update duration display

--- apps/web/src/views/IfosV2/hooks/ifo/useIFODuration.tsx
+++ apps/web/src/views/IfosV2/hooks/ifo/useIFODuration.tsx
@@ -2,18 +2,30 @@ import { useTranslation } from '@pancakeswap/localization'
 import getTimePeriods from '@pancakeswap/utils/getTimePeriods'
 
 export const useIFODuration = (duration: number) => {
-  const { days, hours, minutes } = getTimePeriods(duration, 'ceil')
+  const { days, hours, minutes } = getTimePeriods(duration)
   const { t } = useTranslation()
 
-  if (days > 1) {
-    return `${days} ${t('days')}`
+  if (days > 0) {
+    const dayText = days === 1 ? t('1 day') : `${days} ${t('days')}`
+    if (hours > 0) {
+      const hourText = `${hours} ${t(hours > 1 ? 'hours' : 'hour')}`
+      return `${dayText} ${hourText}`
+    }
+    if (minutes > 0) {
+      return `${dayText} ${minutes} ${t('mins')}`
+    }
+    return dayText
   }
 
-  if (hours >= 1) {
-    return `${hours} ${t(hours > 1 ? 'hours' : 'hour')}`
+  if (hours > 0) {
+    const hourText = hours === 1 ? t('1 hour') : `${hours} ${t('hours')}`
+    if (minutes > 0) {
+      return `${hourText} ${minutes} ${t('mins')}`
+    }
+    return hourText
   }
 
-  if (minutes > 1) {
+  if (minutes > 0) {
     return `${minutes} ${t('mins')}`
   }
 
