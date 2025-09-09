afbf5946de4b28bb6a61948117b483f4690d320f
Chef Jerry <jerry@pancakeswap.com>
2025-09-09 20:21:26 +0800

    feat: remove solana v3 pools modal ui

--- apps/web/src/views/universalFarms/components/PositionActions/SolanaV3PositionActions.tsx
+++ apps/web/src/views/universalFarms/components/PositionActions/SolanaV3PositionActions.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
-import { AddIcon, Button, Flex, IconButton, MinusIcon } from '@pancakeswap/uikit'
+import { AddIcon, Button, Flex, IconButton, MinusIcon, useModalV2 } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
+import { SolanaV3PoolInfo } from 'state/farmsV4/state/type'
+import { SolanaV3PositionDetail } from 'state/farmsV4/state/accountPositions/type'
+import { useCallback } from 'react'
+import SolanaV3RemovePositionModal from '../Modals/SolanaV3RemovePositionModal'
 import { StopPropagation } from '../StopPropagation'
 
 type ActionPanelProps = {
@@ -8,17 +12,33 @@ type ActionPanelProps = {
   outOfRange: boolean
   chainId: number
   detailMode?: boolean
+  poolInfo: SolanaV3PoolInfo | undefined
+  position: SolanaV3PositionDetail
 }
 
-export const SolanaV3PositionActions: React.FC<ActionPanelProps> = ({ removed }) => {
+export const SolanaV3PositionActions: React.FC<ActionPanelProps> = ({ removed, poolInfo, position }) => {
   const { t } = useTranslation()
+
+  const removePositionModal = useModalV2()
+  const handleRemovePositionClick = useCallback(() => {
+    removePositionModal.onOpen()
+  }, [removePositionModal])
+
   return (
     <StopPropagation>
       <ActionPanelContainer>
-        {!removed ? (
-          <IconButton variant="secondary">
-            <MinusIcon color="primary" width="24px" />
-          </IconButton>
+        {!removed && poolInfo?.rawPool ? (
+          <>
+            <IconButton variant="secondary" onClick={handleRemovePositionClick}>
+              <MinusIcon color="primary" width="24px" />
+            </IconButton>
+            <SolanaV3RemovePositionModal
+              isOpen={removePositionModal.isOpen}
+              onClose={removePositionModal.onDismiss}
+              pool={poolInfo}
+              position={position}
+            />
+          </>
         ) : null}
         <IconButton variant="secondary">
           <AddIcon color="primary" width="24px" />
